2cb38e342690f64ac43ad1cfae39effc36d99d21
Dean Van Greunen <deanvg9000@gmail.com>
2021-04-26 12:24:26 +0200

    Added Null Default

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
 
-            $table->string('api_key');
+            $table->string('api_key')->nullable();
         });
     }
 
